0056198bf0cbc6c701b8972c12fdfdda19252eee
Kirubel-Gebre <57181204+Kirubel-Gebre@users.noreply.github.com>
2024-07-01 18:10:03 +0200

    Update peak.py

--- peak.py
+++ peak.py
@@ -1,23 +1,19 @@
-from flask import Flask, jsonify, Response, request, make_response
-from flask_cors import CORS
-# from starlette.middleware.cors import CORSMiddleware
-# from fastapi.responses import StreamingResponse
+from quart import Quart, jsonify, request, make_response
+from quart_cors import cors
 from google.cloud import speech_v1p1beta1 as speech
 from google.cloud import translate_v2 as translate
-# import azure.cognitiveservices.speech as speechsdk
 from openai import OpenAI
 import os
 import io
 import json
-# import tempfile
 import requests
-os.environ["FFPROBE_PATH"] = "/usr/local/bin/ffprobe"
 from pydub import AudioSegment
-AudioSegment.ffprobe = os.environ["FFPROBE_PATH"]
 from scipy.io.wavfile import read as read_wav
-import audioread
 import soundfile as sf
 import numpy as np
+
+os.environ["FFPROBE_PATH"] = "/usr/local/bin/ffprobe"
+AudioSegment.ffprobe = os.environ["FFPROBE_PATH"]
 
 # Define voice options for Azure Text-to-Speech
 voice_options = {
@@ -84,23 +80,9 @@ voice_options = {
     "Chinese (Traditional)": "zh-TW",
 }
 
-app = Flask(__name__)
-CORS(app)
+app = Quart(__name__)
+app = cors(app)
 client = OpenAI()
-
-# origins = [
-#     "https://www.ai-tolk.nl/"
-
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-#     expose_headers=["*"],
-# )
 
 @app.route("/", methods=["POST"])
 async def transcribe_and_translate_audio():
@@ -127,18 +109,11 @@ async def transcribe_and_translate_audio():
     if is_geez == "true":
         transcribed_text = form_data.get('transcribedText', '')
         
-        audio_file = request.files.get("audio", "hello")
-    	# print(audio_file.read())
+        audio_file = request.files.get("audio")
         temp_audio_file = "temp_audio.ogg"
         converted_audio_file = "temp_audio.wav"
         with open(temp_audio_file, "wb") as f:
-            f.write(audio_file.read())
-            
-        # audio_file = request.files.get('audio')
-        # temp_audio_file = "/tmp/temp_audio.ogg"
-        # converted_audio_file = "/tmp/temp_audio.wav"
-        # with open(temp_audio_file, "wb") as f:
-        #     f.write(await audio_file.read())
+            f.write(await audio_file.read())
 
         try:
             audio = AudioSegment.from_file(temp_audio_file, format="ogg")
@@ -152,15 +127,10 @@ async def transcribe_and_translate_audio():
             raise
 
         sampling_rate, data = read_wav(converted_audio_file)
-        # sampling_rate, data = read_wav(temp_audio_file)
-        return (sampling_rate, data)
         
-        
-
         with open(converted_audio_file, "rb") as audio_file:
-        # with open(temp_audio_file, "rb") as audio_file:
             content = audio_file.read()
-        # Prepare the RecognitionAudio and RecognitionConfig objects
+            
         audio = speech.RecognitionAudio(content=content)
         config = speech.RecognitionConfig(
             encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
@@ -168,33 +138,25 @@ async def transcribe_and_translate_audio():
             language_code="am-ET",
         )
 
-        # Perform the transcription
         gc_client = speech.SpeechClient()
         response = gc_client.recognize(config=config, audio=audio)
 
-
         transcribed_text = ""
-        # Print the transcription results
         for result in response.results:
-            # print(result.alternatives[0].transcript)
             transcribed_text += result.alternatives[0].transcript
         
     else:
-        audio_file = request.files.get("audio", "hello")
-    
+        audio_file = request.files.get("audio")
         temp_audio_file = "temp_audio.wav"
         with open(temp_audio_file, "wb") as f:
-            f.write(audio_file.read())
+            f.write(await audio_file.read())
 
-        # Use Whisper for transcription
-        transcribed_text = transcribe_whisper(temp_audio_file)
+        transcribed_text = await transcribe_whisper(temp_audio_file)
 
     if is_geez == "true" or target_language in ["Amharic", "Tigrinya"]:
-        # Translate using Google Cloud Translation for Amharic or Tigrinya
         translated_text = translate_text(transcribed_text, target_language=voice_options[target_language], source_language=voice_options[source_language])
     else:
-        # Use OpenAI chat completions for translation
-        response = client.chat.completions.create(
+        response = await client.chat.completions.create(
             model="gpt-3.5-turbo-0301",
             messages=[
                 {
@@ -204,7 +166,7 @@ async def transcribe_and_translate_audio():
                 {
                     "role": "user",
                     "content": transcribed_text
-                } 
+                }
             ],
             max_tokens=2048,
             n=1,
@@ -213,16 +175,10 @@ async def transcribe_and_translate_audio():
         )
         translated_text = response.choices[0].message.content
         
-        # Cleanup temporary audio file
         os.remove(temp_audio_file)
-
-    # Synthesize translated text to audio using Azure TTS
 
     audio_bytes = synthesize_audio(translated_text, language=voice_options[target_language])
 
-    
-
-    # Prepare response data
     response_data = {
         "sourceLanguage": source_language,
         "targetLanguage": target_language,
@@ -231,25 +187,16 @@ async def transcribe_and_translate_audio():
     }
     json_data = json.dumps(response_data)
 
-    # return json_data
-    
-    # Create the Flask response
     response = make_response(json_data + "\n")
     response.set_data(response.get_data() + audio_bytes)
-    response.headers.set('Content-Type', 'application/json')
-    response.headers.set('Audio-Type', 'audio/mpeg')  # Assuming the audio is in MP3 format
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Audio-Type'] = 'audio/mpeg'
     
     return response
-    # Return response with JSON and audio bytes
-    # return Response(
-    #     content=json_data.encode() + b'\n' + audio_bytes,
-    #     media_type="application/octet-stream"
-    # )
 
-def transcribe_whisper(audio_file_path: str) -> str:
-    """Transcribes audio using Whisper from OpenAI."""
+async def transcribe_whisper(audio_file_path: str) -> str:
     with open(audio_file_path, "rb") as audio:
-        transcript = client.audio.transcriptions.create(
+        transcript = await client.audio.transcriptions.create(
             model="whisper-1",
             file=audio
         )
@@ -257,18 +204,14 @@ def transcribe_whisper(audio_file_path: str) -> str:
     return transcript.text
 
 def translate_text(text: str, target_language: str, source_language: str) -> str:
-    """Translates text using Google Cloud Translation."""
     translate_client = translate.Client()
     result = translate_client.translate(text, target_language=target_language, source_language=source_language)
-
     return result['translatedText']
 
 def synthesize_audio(text: str, language: str) -> bytes:
-    """Synthesizes speech audio using Azure Text-to-Speech."""
     subscription_key = "e13bd4b8a3ad44e986342a0f7fe2dabc"
     region = "westeurope"
 
-    # API endpoint
     base_url = f"https://{region}.tts.speech.microsoft.com/"
     path = "cognitiveservices/v1"
     constructed_url = base_url + path
@@ -284,8 +227,7 @@ def synthesize_audio(text: str, language: str) -> bytes:
     else:
         print(f"Error fetching voices: {response.status_code}, {response.text}")
         voice = ""
-        
-    # SSML (Speech Synthesis Markup Language) template
+
     ssml_template = f"""
     <speak version='1.0' xml:lang='{language}'>
         <voice name='{voice}'>
@@ -294,10 +236,8 @@ def synthesize_audio(text: str, language: str) -> bytes:
     </speak>
     """
 
-    # Prepare the SSML
     ssml = ssml_template.format(text=text)
 
-    # Set up the HTTP headers
     headers = {
         "Ocp-Apim-Subscription-Key": subscription_key,
         "Content-Type": "application/ssml+xml",
@@ -305,20 +245,10 @@ def synthesize_audio(text: str, language: str) -> bytes:
         "User-Agent": "YourAppName"
     }
 
-    # Send the request to Azure
     response = requests.post(constructed_url, headers=headers, data=ssml.encode("utf-8"))
-   
 
     if response.status_code == 200:
-        # Convert the response content to an AudioSegment
-        # audio = AudioSegment.from_file(io.BytesIO(response.content), format="mp3")
-        
-        # # Export as WAV to bytes
-        # buffer = io.BytesIO()
-        # audio.export(buffer, format="wav")
-        # audio_bytes = buffer.getvalue()
         audio_bytes = response.content
-        
         print(f"Audio generated successfully for: {text}")
         return audio_bytes
     else:
@@ -326,8 +256,6 @@ def synthesize_audio(text: str, language: str) -> bytes:
         print(f"Reason: {response.reason}")
         print(response.text)
         return None
-
-
 
 if __name__ == "__main__":
     app.run()
